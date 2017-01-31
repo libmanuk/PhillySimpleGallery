e46ccd50c48b3e4fb92542e821a90a36c0047fe4
Eric C. Weig <eweig@email.uky.edu>
2017-01-31 07:27:19 -0500

    Create IndexController.php

---
+++ controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Eric C. Weig, 2017
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package SimpleSplashForm
+ */
+
+/**
+ * Controller for Splash form.
+ *
+ * @package SimpleSplashForm
+ */
+class SimpleSplashForm_IndexController extends Omeka_Controller_AbstractActionController
+{
+    public function indexAction()
+    {
+ 
+    }
+}
