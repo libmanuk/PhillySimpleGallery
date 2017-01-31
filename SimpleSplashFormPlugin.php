561c3ec6e9da6a33a80192098e6e488ffc6fc0ee
Eric C. Weig <eweig@email.uky.edu>
2017-01-31 07:26:15 -0500

    Add files via upload

---
+++ SimpleSplashFormPlugin.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright Eric C. Weig, 2017
+ * @package SimpleSplashForm
+ */
+
+/**
+ * SimpleSplashForm plugin class
+ *
+ * @copyright Eric C. Weig, 2017
+ * @package SimpleSplashForm
+ */
+// Define Constants.
+define('SIMPLE_SPLASH_FORM_PAGE_PATH', 'splashpg/');
+define('SIMPLE_SPLASH_FORM_SPLASH_PAGE_TITLE', 'Splash Page');
+define('SIMPLE_SPLASH_FORM_SPLASH_PAGE_INSTRUCTIONS', 'Include an introduction for the page here.');
+define('SIMPLE_SPLASH_FORM_ADD_TO_MAIN_NAVIGATION', 1);
+
+
+class SimpleSplashFormPlugin extends Omeka_Plugin_AbstractPlugin
+{
+    // Define Hooks
+    protected $_hooks = array(
+        'install',
+        'uninstall',
+        'define_routes',
+        'config_form',
+        'config'
+    );
+
+    //Add filters
+    protected $_filters = array(
+        'public_navigation_main'
+    );
+
+   public function hookInstall()
+    {
+        set_option('simple_splash_form_splash_page_title', SIMPLE_SPLASH_FORM_SPLASH_PAGE_TITLE);
+        set_option('simple_splash_form_splash_page_instructions', SIMPLE_SPLASH_FORM_SPLASH_PAGE_INSTRUCTIONS);    
+        set_option('simple_splash_form_add_to_main_navigation', SIMPLE_SPLASH_FORM_ADD_TO_MAIN_NAVIGATION);    
+    }
+
+    public function hookUninstall()
+    {
+        delete_option('simple_splash_form_splash_page_title');
+        delete_option('simple_splash_form_splash_page_instructions');
+        delete_option('simple_splash_form_add_to_main_navigation');    
+    }
+
+
+    function hookDefineRoutes($args)
+    {
+        $router = $args['router'];
+        $router->addRoute(
+            'simple_splash_form_form', 
+            new Zend_Controller_Router_Route(
+                SIMPLE_SPLASH_FORM_PAGE_PATH, 
+                array('module'       => 'simple-splash-form')
+            )
+        );
+
+    }
+
+    public function hookConfigForm() 
+    {
+        include 'config_form.php';
+    }
+
+    public function hookConfig($args)
+    {
+        $post = $args['post'];
+        set_option('simple_splash_form_splash_page_title', $post['splash_page_title']);
+        set_option('simple_splash_form_splash_page_instructions',$post['splash_page_instructions']);
+        set_option('simple_splash_form_add_to_main_navigation', $post['add_to_main_navigation']);
+    }
+
+    public function filterPublicNavigationMain($nav)
+    {
+        $splash_title = get_option('simple_splash_form_splash_page_title');
+        $splash_add_to_navigation = get_option('simple_splash_form_add_to_main_navigation');
+        if ($splash_add_to_navigation) {
+                $nav[] = array(
+                    'label'   => $splash_title,
+                    'uri'     => url(array(),'simple_splash_form_form'),
+                    'visible' => true
+                );
+        }
+        return $nav;
+    }
+}
---
+++ config_form.php
@@ -0,0 +1,52 @@
+<?php echo js_tag('vendor/tiny_mce/tiny_mce'); ?>
+<script type="text/javascript">
+jQuery(window).load(function () {
+    Omeka.wysiwyg({
+        mode: 'specific_textareas',
+        editor_selector: 'html-editor'
+    });
+});
+</script>
+
+<?php
+$splash_page_title                      = get_option('simple_splash_form_splash_page_title');
+$splash_page_instructions               = get_option('simple_splash_form_splash_page_instructions');
+$add_to_main_navigation                  = get_option('simple_splash_form_add_to_main_navigation');
+$view = get_view();
+?>
+
+
+<div class="field">
+    <?php echo $view->formLabel('splash_page_title', 'Splash Page Title'); ?>
+    <div class="inputs">
+        <?php echo $view->formText('splash_page_title', $splash_page_title, array('class' => 'textinput')); ?>
+        <p class="explanation">
+            The title of the splash page (not HTML).
+        </p>
+    </div>
+</div>
+
+<div class="field">
+    <?php echo $view->formLabel('splash_page_instructions', 'Introduction for Splash Page'); ?>
+    <div class="inputs">
+        <?php echo $view->formTextarea('splash_page_instructions', $splash_page_instructions, array('rows' => '10', 'cols' => '60', 'class' => array('textinput', 'html-editor'))); ?>
+        <p class="explanation">
+            An introduction to add to the splash page.
+        </p>
+    </div>
+</div>
+
+<div class="field">
+    <?php echo $view->formLabel('add_to_main_navigation', 'Add to Main Navigation'); ?>
+    <div class="inputs">
+        <?php echo $view->formCheckbox('add_to_main_navigation', $add_to_main_navigation, null, array('1', '0')); ?>
+        <p class="explanation">
+            If checked, add a link to the splash page to the main site
+            navigation.
+        </p>
+    </div>
+</div>
+
+
+
+
